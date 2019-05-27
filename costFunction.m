64c1637f30c98267f38aa768fd0454368d5c5396
gpx <1457495424@qq.com>
2019-05-27 11:42:53 +0800

    delete some files

--- costFunction.m
+++
@@ -1,8 +0,0 @@
-function [jVal, gradient] = costFunction(theta)
-  
-  jVal = (theta(1) - 5) ^ 2 + (theta(2) - 5) ^ 2;      % 代价函数
-  
-  gradient = zeros(2, 1);
-  gradient(1) = 2 * (theta(1) - 5);                    % 代价函数对第一个参数的偏导
-  gradient(2) = 2 * (theta(2) - 5);                    % 代价函数对第二个参数的偏导
-endfunction
--- test_costFunction.m
+++
@@ -1,3 +0,0 @@
-options = optimset('GradObj', 'on', 'MaxIter', '100');
-initialTheta = zeros(2, 1);         % 给参数设定一个初值
-[optTheta, functionVal, exutFlag] = fminunc(@costFunction, initialTheta, options)
